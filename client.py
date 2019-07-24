902f9b6f324693171717963f5b491584b75ac611
gamburgm <mitch.gamburg@gmail.com>
2019-07-23 23:46:41 -0400

    clean up client

--- client.py
+++ client.py
@@ -2,7 +2,7 @@
 
 import socket
 
-#this came from https://realpython.com/python-sockets
+# This code is based on the simple client from https://realpython.com/python-sockets
 
 HOST = '172.20.10.3'  # The server's hostname or IP address
 PORT = 3333        # The port used by the server
@@ -10,8 +10,7 @@ PORT = 3333        # The port used by the server
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     while True:
-        message = input().encode()
+        message = input('> ').encode()
         s.sendall(message)
         data = s.recv(1024)
-        
 

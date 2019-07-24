2c797e9f6c123b933eed25a558f3861f9c95169a
Mitchell Gamburg <mitch.gamburg@gmail.com>
2019-07-23 22:26:32 -0400

    debugging garbage server details and indentations

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ import types
 # goal: get a server that connect with multiple clients and get it to send messages from one client to the others
 sockets_container = selectors.DefaultSelector()
 host = '0.0.0.0'
-port = 3333
+port = 3335
 
 
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -25,17 +25,10 @@ try:
 		ready_sockets = sockets_container.select()
 		for socket_obj, events in ready_sockets:
 			if socket_obj.data is None:
-				conn, addr = socket_obj.fileobj.accept()  
-				print('accepted connection from', addr)
-				conn.setblocking(False)
-				events = selectors.EVENT_READ | selectors.EVENT_WRITE
-				sel.register(conn, events)
-				"""
-					client_socket, address = socket_obj.fileobj.accept()
-					client_socket.setblocking(False)
-					sockets_container.register(client_socket, selectors.EVENT_READ | selectors.EVENT_WRITE)
-					list_of_sockets.append(client_socket)
-				"""
+				client_socket, addr = socket_obj.fileobj.accept()  
+				client_socket.setblocking(False)
+				sockets_container.register(client_socket, selectors.EVENT_READ | selectors.EVENT_WRITE)
+				list_of_sockets.append(client_socket)
 			else:
 				handle_client(socket_obj.fileobj, events)
 
@@ -63,6 +56,3 @@ def handle_client(socket_file, events):
 1. send it to everybody except yourself
 2. route to a specific IP address specified on the command line with an option
 """
-
-
-

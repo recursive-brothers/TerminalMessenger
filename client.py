6493973965d01fe84accdcbeb523f2bc07d804d6
zumaad <khanzumaad@gmail.com>
2019-07-23 22:13:16 -0400

    dummy client

---
+++ client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import socket
+
+HOST = '172.20.10.3'  # The server's hostname or IP address
+PORT = 3333        # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        message = input().encode()
+        s.sendall(message)
+        data = s.recv(1024)
+        
+

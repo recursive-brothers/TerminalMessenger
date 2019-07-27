0b7bd7f5f70f9460d5a5b664f2690cc0d5d8cdad
gamburgm <mitch.gamburg@gmail.com>
2019-07-27 16:55:35 -0400

    adding back asyncio code commented out

--- client.py
+++ client.py
@@ -20,3 +20,22 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         data = s.recv(1024)
         print(data)
 
+# async def async_input(server_socket):
+#     while True:
+#         message = await ainput("> ")
+#         server_socket.sendall(message)
+
+# async def get_messages(server_socket):
+#     while True:
+#         received_message = server_socket.recv(1024)
+#         if received_message:
+#             print(received_message)
+
+
+
+
+# async def main(s):
+#     get_input = asyncio.create_task(async_input(s))
+#     get_output = asyncio.create_task(get_messages(s))
+#     await get_input
+#     await get_output

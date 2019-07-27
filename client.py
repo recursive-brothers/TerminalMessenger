9338c11a4f4b5d0eea6b1ba184149cb0fdb964f9
gamburgm <mitch.gamburg@gmail.com>
2019-07-27 15:14:56 -0400

    add command line port to client.py

--- client.py
+++ client.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 
 import socket
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('port')
+args = parser.parse_args()
 
 # This code is based on the simple client from https://realpython.com/python-sockets
 
 HOST = '172.20.10.3'  # The server's hostname or IP address
-PORT = 3335       # The port used by the server
+PORT = int(args.port) # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))

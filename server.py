292bc1374da70d7724cf65157917dcdd9f00cdbc
zumaad <khanzumaad@gmail.com>
2019-07-28 20:09:28 -0400

    Added logging

--- server.py
+++ server.py
@@ -6,6 +6,12 @@ import types
 import argparse
 import datetime
 import traceback
+import logging
+
+logging.basicConfig(filename='server.log',
+                            filemode='a',
+                            datefmt='%H:%M:%S',
+                            level=logging.DEBUG)
 
 parser = argparse.ArgumentParser()
 parser.add_argument('port')
@@ -20,7 +26,7 @@ port = int(args.port)
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
-print('listening on', (host, port))
+logging.debug('listening on', (host, port))
 lsock.setblocking(False)
 sockets_container.register(lsock, selectors.EVENT_READ, data=None)
 
@@ -32,19 +38,19 @@ def handle_client(client_socket, events):
 	if events & selectors.EVENT_READ:
 		recv_data = socket_obj.recv(1024)
 		if not recv_data:
-			print('closing connection', client_socket.data)
+			logging.debug('closing connection', client_socket.data)
 			sockets_container.unregister(socket_obj)
 			socket_obj.close()
 			list_of_sockets.remove(socket_obj)
 			return
 
-		print("data just received")
-		print("client sent -> ", recv_data)
+		logging.debug("data just received")
+		logging.debug("client sent -> ", recv_data)
 	
 	if recv_data:
 		for socket in list_of_sockets:
 			if socket != socket_obj:
-				print("sending to other clients")
+				logging.debug("sending to other clients")
 				socket.send(recv_data)
 
 def main():
@@ -53,7 +59,7 @@ def main():
 		for socket_obj, events in ready_sockets:
 			if socket_obj.data is None:
 				client_socket, addr = socket_obj.fileobj.accept() 
-				print('accepted client', addr) 
+				logging.debug('accepted client', addr) 
 				client_socket.setblocking(False)
 				sockets_container.register(client_socket, selectors.EVENT_READ | selectors.EVENT_WRITE, data = addr)
 				list_of_sockets.append(client_socket)
@@ -61,24 +67,10 @@ def main():
 				handle_client(socket_obj, events)
 
 
-print(f'----------------------STARTING SESSION {datetime.datetime.now()}----------------------')
+logging.debug('----------------------STARTING SESSION----------------------')
 try:
 	main()
 except Exception as e:
 	traceback.print_exc()
 
-print(f'----------------------ENDING SESSION {datetime.datetime.now()}------------------------')
-
-
-
-
-"""
-1. send it to everybody except yourself
-2. route to a specific IP address specified on the command line with an option
-"""
-
-
-"""
-Problem: we are just printing a lt sign and then any text interaction follows from that
-Solution: pre- and post-pend every message with a new line, and then additionally append with a caret
-"""
+logging.debug('----------------------ENDING SESSION-------------------------')

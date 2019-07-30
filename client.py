2e85dd7fb54f2cd47d1dfa8e323fb0104d398b63
zumaad <khanzumaad@gmail.com>
2019-07-29 20:30:27 -0400

    added curses client

--- client.py
+++ client.py
@@ -4,6 +4,15 @@ import socket
 import argparse
 import asyncio
 from aioconsole import ainput
+import curses
+import logging
+
+logging.basicConfig(filename='server.log',
+                            filemode='a',
+                            datefmt='%H:%M:%S',
+                            level=logging.DEBUG)
+
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('port')
@@ -31,16 +40,76 @@ async def get_messages(server_socket):
             print('\n' + received_message + '\n> ', end='')
         await asyncio.sleep(.1)
           
-async def main(s):
+async def background_tasks(s):
     get_input = asyncio.ensure_future(get_user_input(s))
     get_output = asyncio.ensure_future(get_messages(s))
     await get_output
     await get_input
     
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((HOST, PORT))
-    s.setblocking(False)
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main(s))
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.connect((HOST, PORT))
+#     s.setblocking(False)
+#     loop = asyncio.get_event_loop()
+#     loop.run_until_complete(background_tasks(s))
     
+
+
+def main(stdscr):
+    # curses.echo()
+    num_rows, num_cols = stdscr.getmaxyx()
+    
+
+    received_messages_rows = int(.85 * num_rows)
+    sending_message_rows = num_rows - received_messages_rows
+
+    received_window = curses.newwin(received_messages_rows,num_cols,0,0)
+    received_window.border('|', '|', '-', '-', '+', '+', '+', '+')
+    received_window.refresh()
+
+    input_window = curses.newwin(sending_message_rows,num_cols,received_messages_rows,0)
+    input_window.border('|', '|', '-', '-', '+', '+', '+', '+')
+    input_window.refresh()
+
+    input_window.nodelay(True)
+    received_window.nodelay(True)
+    
+    built_str = ''
+    y_position = 1
+    x_position = 1
+    while True:
+
+        ch = input_window.getch(y_position,x_position)
+
+        if ch != curses.ERR:
+            if ch == ord('\n'):
+                input_window.erase()
+                input_window.border('|', '|', '-', '-', '+', '+', '+', '+')
+                input_window.refresh()
+                received_window.addstr(1,1,built_str)
+                received_window.refresh()
+                x_position = y_position = 1
+                built_str = ''
+
+            elif ch == 127:
+                input_window.addstr(y_position,x_position - 1,"  ")
+                input_window.refresh()
+                x_position -= 1
+            
+            else:
+                built_str += chr(ch)
+                input_window.addstr(y_position,x_position,chr(ch))
+                x_position += 1 
+
+        if x_position == num_cols - 1:
+            y_position += 1
+            x_position = 1
+        if x_position == 0:
+            if y_position != 1:
+                y_position -= 1
+                x_position = num_cols - 2
+            else:
+                x_position = 1    
+
+
+curses.wrapper(main)

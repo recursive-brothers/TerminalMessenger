fe64187c8a5543e2087b6919577d4011a4c8e64e
gamburgm <mitch.gamburg@gmail.com>
2019-07-29 20:46:33 -0400

    server works

--- server.py
+++ server.py
@@ -7,6 +7,8 @@ import argparse
 import datetime
 import traceback
 import logging
+import datetime
+
 
 logging.basicConfig(filename='server.log',
                             filemode='a',
@@ -26,7 +28,7 @@ port = int(args.port)
 lsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 lsock.bind((host, port))
 lsock.listen()
-logging.debug('listening on', (host, port))
+logging.debug('listening on' + str((host, port)) + f' {str(datetime.datetime.now())}')
 lsock.setblocking(False)
 sockets_container.register(lsock, selectors.EVENT_READ, data=None)
 
@@ -38,19 +40,18 @@ def handle_client(client_socket, events):
 	if events & selectors.EVENT_READ:
 		recv_data = socket_obj.recv(1024)
 		if not recv_data:
-			logging.debug('closing connection', client_socket.data)
+			logging.debug('closing connection ' + client_socket.data + f' {str(datetime.datetime.now())}')
 			sockets_container.unregister(socket_obj)
 			socket_obj.close()
 			list_of_sockets.remove(socket_obj)
 			return
 
-		logging.debug("data just received")
-		logging.debug("client sent -> " + recv_data.decode())
+		logging.debug("client sent -> " + recv_data.decode() + f' {str(datetime.datetime.now())}')
 	
 	if recv_data:
 		for socket in list_of_sockets:
 			if socket != socket_obj:
-				logging.debug("sending to other clients")
+				logging.debug(f'sending to other clients {str(datetime.datetime.now())}')
 				socket.send(recv_data)
 
 def main():
@@ -59,7 +60,7 @@ def main():
 		for socket_obj, events in ready_sockets:
 			if socket_obj.data is None:
 				client_socket, addr = socket_obj.fileobj.accept() 
-				logging.debug('accepted client', addr) 
+				logging.debug('accepted client' + addr + f' {str(datetime.datetime.now())}') 
 				client_socket.setblocking(False)
 				sockets_container.register(client_socket, selectors.EVENT_READ | selectors.EVENT_WRITE, data = addr)
 				list_of_sockets.append(client_socket)
@@ -67,10 +68,10 @@ def main():
 				handle_client(socket_obj, events)
 
 
-logging.debug('----------------------STARTING SESSION----------------------')
+logging.debug(f'----------------------STARTING SESSION {str(datetime.datetime.now())}----------------------')
 try:
 	main()
 except Exception as e:
-	traceback.print_exc()
+	logging.debug(traceback.format_exc())
 
-logging.debug('----------------------ENDING SESSION-------------------------')
+logging.debug(f'----------------------ENDING SESSION {str(datetime.datetime.now())}-------------------------')

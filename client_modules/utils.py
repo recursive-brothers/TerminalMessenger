06d8c4aa949a03c25fdb1e4c88864698ab2ba480
zumaad <khanzumaad@gmail.com>
2019-09-01 18:58:24 -0400

    type hinting

--- client_modules/utils.py
+++ client_modules/utils.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from typing import Any
 
 ADDRESS = None
 
@@ -19,34 +20,33 @@ class SENDER(Enum):
     OTHER    = 3
 
 class StringBuilder:
-    def __init__(self,string = None):
+    def __init__(self, string: str = None):
         self.ch_list = list(string) if string else []
         
-    
-    def _append(self, seq):
+    def _append(self, seq: str) -> None:
         self.ch_list.extend(list(seq))
     
-    def delete(self, num):
+    def delete(self, num: int) -> None:
         if num <= 0:
             return
         self.ch_list = self.ch_list[:num * -1]
 
-    def build(self):
+    def build(self) -> str:
         built_str = ''.join(self.ch_list)
         self.ch_list.clear()
         return built_str
 
-    def __iadd__(self, seq):
+    def __iadd__(self, seq: str) -> 'StringBuilder':
         self._append(seq)
         return self
         
-    def __bool__(self):
+    def __bool__(self) -> bool:
         return bool(self.ch_list)
 
-    def __eq__(self, other):
-        return other.ch_list == self.ch_list
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, StringBuilder) and other.ch_list == self.ch_list
 
 class CursorPosition:
-    def __init__(self,startY,startX):
+    def __init__(self, startY: int, startX: int):
         self.y = startY
         self.x = startX

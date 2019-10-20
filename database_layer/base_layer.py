1afd7d1e125f321e7a44262ea97f494116f0d33b
zumaad <khanzumaad@gmail.com>
2019-10-20 17:25:08 -0400

    created base class for db interfacing

---
+++ database_layer/base_layer.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+def BaseLayer(ABC):
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
+    def get_chatroom_msgs(self, chatroom_id, limit = 50):
+        pass
+    
+    @abstractmethod
+    def insert_msg(self, msg, chatroom_id):
+        pass

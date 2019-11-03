7861d379e00168596c380d755346508c33643ec8
zumaad <khanzumaad@gmail.com>
2019-11-03 18:07:39 -0500

    debug

--- server.py
+++ server.py
@@ -97,6 +97,7 @@ def os_error_logging(socket_wrapper) -> None:
 
 def load_messages(socket_wrapper) -> None:
     results = db_client.get_chatroom_msgs(CHATROOM_ID, 50)
+    log_debug_info(results)
     json_messages = json.dumps(results)
     socket_wrapper.fileobj.send(''.join(reversed(json_messages)).encode())
 

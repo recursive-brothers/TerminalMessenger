a4c1b1ac5e52164866caadeab63c407457d39725
zumaad <khanzumaad@gmail.com>
2019-11-03 18:26:01 -0500

    boto logs way too much

--- server.py
+++ server.py
@@ -37,11 +37,14 @@ args = parser.parse_args()
 logging.basicConfig(filename='server.log',
                             filemode='a',
                             datefmt='%H:%M:%S',
-                            level=logging.INFO)
+                            level=logging.DEBUG)
 
 list_of_sockets: List[socket.socket] = []
 client_manager = selectors.DefaultSelector()
 
+logging.getLogger('boto3').setLevel(logging.WARNING)
+logging.getLogger('botocore').setLevel(logging.WARNING)
+logging.getLogger('nose').setLevel(logging.WARNING)
 
 class ClientInformation:
     def __init__(self, addr: str):
@@ -52,7 +55,7 @@ class ClientInformation:
 def log_debug_info(*args: Any) -> None:
     str_args = [str(arg) for arg in args]
     str_args.append(str(datetime.datetime.now()))
-    logging.info(' '.join(str_args))
+    logging.debug(' '.join(str_args))
 
 def initialize_master_socket(port: int) -> None:
     master_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)

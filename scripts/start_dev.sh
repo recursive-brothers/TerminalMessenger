0233dbfaac51eec67e8b05386d97c8361c463030
gamburgm <mitch.gamburg@gmail.com>
2019-12-05 22:39:45 -0500

    trying this pattern, not sure how else to do it

--- scripts/start_dev.sh
+++ scripts/start_dev.sh
@@ -1,53 +1,9 @@
 #!/bin/bash
 
-# what needs to happen?
-# the pattern is:
-# 1. somebody pushes code from a branch
-# 2. we get that branch name and execute the below code with that branch name on the server
-# 3. I think there needs to be one script that kicks it off and the other that does the server work
-
-
 BRANCH=$(git symbolic-ref HEAD 2>/dev/null)
 BRANCH=${BRANCH##refs/heads/}
 
-DIRNAME="dev_${BRANCH//-/_}"
-RESERVED_BIT="-1"
-
-if [ ! -d "${DIRNAME}" ]; then
-    BITMAP=$(head -n 1 bitmap.txt)
-
-    if [ $BITMAP == "" ]; then BITMAP="0"; fi
-
-    if [ $BITMAP == "1023" ]; then
-        echo "No dev slots remain!"
-        exit
-    fi
-
-    for i in {0..9}; do
-        if [ $(( BITMAP & ( 1 << i ) )) -eq 0 ]; then
-            (( BITMAP |= ( 1 << i ) ))
-            echo $BITMAP > bitmap.txt
-            RESERVED_BIT=$i
-            echo $RESERVED_BIT > bit.txt
-            echo "Looks like we end at ${i}!!!"
-            break
-        fi
-    done
-else 
-    RESERVED_BIT=$(head -n 1 bit.txt)
-fi
-
-
-let PORT="3334 + RESERVED_BIT"
-echo "Reserved port is: ${PORT}"
-
-mkdir $DIRNAME
-cd $DIRNAME
-
-git clone https://github.com/recursive-brothers/TerminalMessenger.git
-git checkout $BRANCH
-git pull
-
-screen -X -S "dev${RESERVED_BIT}" quit
-screen -S "dev${RESERVED_BIT}" -m -d ./server.py $PORT
-
+ssh tm@terminalmessenger.com "
+cd TerminalMessenger/;
+./scripts/start_dev_helper.sh ${BRANCH}
+"
---
+++ scripts/start_dev_helper.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+BRANCH=$1
+
+DIRNAME="dev_${BRANCH//-/_}"
+RESERVED_BIT=-1
+
+if [ ! -d "../${DIRNAME}" ]; then
+    BITMAP=$(head -n 1 bitmap.txt)
+
+    if [ $BITMAP == "" ]; then BITMAP="0"; fi
+
+    if [ $BITMAP == "1023" ]; then
+        echo "No dev slots remain!"
+        exit
+    fi
+
+    for i in {0..9}; do
+        if [ $(( BITMAP & ( 1 << i ) )) -eq 0 ]; then
+            (( BITMAP |= ( 1 << i ) ))
+            echo $BITMAP > bitmap.txt
+            RESERVED_BIT=$i
+            echo $RESERVED_BIT > bit.txt
+            echo "Looks like we end at ${i}!!!"
+            break
+        fi
+    done
+
+    mkdir "../${DIRNAME}"
+    cd "../${DIRNAME}"
+    git clone https://github.com/recursive-brothers/TerminalMessenger.git
+else 
+    RESERVED_BIT=$(head -n 1 bit.txt)
+    cd "../${DIRNAME}"
+fi
+
+let PORT="3334 + RESERVED_BIT"
+echo "Reserved port is: ${PORT}"
+
+cd TerminalMessenger
+git checkout $BRANCH
+git pull
+
+screen -X -S $DIRNAME quit
+screen -S $DIRNAME -m -d ./server.py $PORT

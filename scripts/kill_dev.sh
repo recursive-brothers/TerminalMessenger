e7273035f8eae7230a53b2ad24e7b7fcb626e503
gamburgm <mitch.gamburg@gmail.com>
2019-12-07 18:54:18 -0500

    finished, much cleaner

--- scripts/kill_dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-BRANCH=$(git symbolic-ref HEAD 2>/dev/null)
-BRANCH=${BRANCH##refs/heads/}
-
-ssh tm@terminalmessenger.com "
-cd TerminalMessenger/;
-./scripts/kill_dev_helper.sh ${BRANCH}
-"
